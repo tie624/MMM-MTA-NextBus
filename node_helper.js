b49cce88e2dc62d6208f6069e78daaa9c2a58306
Tie <tie624@gmail.com>
2017-09-19 23:54:50 -0400

    New Template

---
+++ node_helper.js
@@ -0,0 +1,47 @@
+/* Magic Mirror
+ * Node Helper: MMM-MTA-NextBus
+ *
+ * By 
+ * MIT Licensed.
+ */
+
+var NodeHelper = require("node_helper");
+
+module.exports = NodeHelper.create({
+
+	// Override socketNotificationReceived method.
+
+	/* socketNotificationReceived(notification, payload)
+	 * This method is called when a socket notification arrives.
+	 *
+	 * argument notification string - The identifier of the noitication.
+	 * argument payload mixed - The payload of the notification.
+	 */
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === "MMM-MTA-NextBus-NOTIFICATION_TEST") {
+			console.log("Working notification system. Notification:", notification, "payload: ", payload);
+			// Send notification
+			this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
+		}
+	},
+
+	// Example function send notification test
+	sendNotificationTest: function(payload) {
+		this.sendSocketNotification("MMM-MTA-NextBus-NOTIFICATION_TEST", payload);
+	},
+
+	// this you can create extra routes for your module
+	extraRoutes: function() {
+		var self = this;
+		this.expressApp.get("/MMM-MTA-NextBus/extra_route", function(req, res) {
+			// call another function
+			values = self.anotherFunction();
+			res.send(values);
+		});
+	},
+
+	// Test another function
+	anotherFunction: function() {
+		return {date: new Date()};
+	}
+});
