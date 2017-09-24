ad5a4b94d4d357a45f1c830a99a9ca4c1d104038
Tie <tie624@gmail.com>
2017-09-23 23:52:04 -0400

    Moved getData to node helper

--- MMM-MTA-NextBus.js
+++ MMM-MTA-NextBus.js
@@ -31,43 +31,7 @@ Module.register("MMM-MTA-NextBus", {
 		}, this.config.updateInterval);
 	},
 
-	/*
-	 * getData
-	 * function example return data and show it in the module wrapper
-	 * get a URL request
-	 *
-	 */
-	getData: function() {
-		var self = this;
-
-		var urlApi = "http://bustime.mta.info/api/siri/stop-monitoring.json?key=" + 
-			self.config.apiKey + "&version=2&OperatorRef=MTA&MonitoringRef=" + 
-			self.config.busStopCode;
-		//var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
-		var retry = true;
-
-		var dataRequest = new XMLHttpRequest();
-		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
-			if (this.readyState === 4) {
-				console.log(this.status);
-				if (this.status === 200) {
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					Log.error(self.name, "Could not load data.");
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		dataRequest.send();
-	},
+	
 
 
 	/* scheduleUpdate()
@@ -150,50 +114,7 @@ Module.register("MMM-MTA-NextBus", {
 		this.sendSocketNotification("MMM-MTA-NextBus-NOTIFICATION_TEST", data);
 	},
 
-	processActionNextBus: function(response) {
-		
-		var result = '';
-		
-		var serviceDelivery = response.Siri.ServiceDelivery;
-		var updateTimestampReference = new Date(serviceDelivery.ResponseTimestamp);
-		
-		//console.log(updateTimestampReference);
-		
-		// array of buses
-		var visits = serviceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit;
-		var visitsCount = Math.min(visits.length, this.config.maxEntries);
-		
-		for (var i = 0; i < visitsCount; i++) {
-			var journey = visits[i].MonitoredVehicleJourney;
-			var line = journey.PublishedLineName[0]; 
-			
-			if (i === 0) {
-				result += 'The next bus is ';
-			} else {
-				result += ' The following bus is ';
-			}
-			
-			var destinationName = journey.DestinationName[0];
-			if (destinationName.startsWith('LIMITED')) {
-				line += ' LIMITED';
-			}
-			
-			result += line + ', ';
-			
-			var monitoredCall = journey.MonitoredCall;
-			// var expectedArrivalTime = new Date(monitoredCall.ExpectedArrivalTime);
-			if (monitoredCall.ExpectedArrivalTime) {
-				var mins = getArrivalEstimateForDateString(monitoredCall.ExpectedArrivalTime, updateTimestampReference);
-				result += mins + ', ';
-			}
-			
-			
-			var distance = monitoredCall.ArrivalProximityText;
-			result += distance + '.';
-		}
-		
-		return result;
-	},
+	
 
 	// socketNotificationReceived from helper
 	socketNotificationReceived: function (notification, payload) {
